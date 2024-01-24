c2232409030219f0ea4cc171c80d50fc897c37a0
pjy <nihoo4u@gmail.com>
2024-01-24 17:14:56 +0900

    if

--- day01.cpp
+++ day01.cpp
@@ -11,12 +11,15 @@ using namespace std;
 int main(void){
     int a;
     cin>>a;
-    switch(a){
-        case 0: cout<<0<<endl;
-        case 1: cout<<1<<endl;
-        case 2: cout<<2<<endl;
-        case 3: cout<<3<<endl;
+    cout<<fixed<<setprecision(2);
+    if (a>300000000){
+        cout<<"cost*0.5 -> "<<a*0.5;
 
+    }
+    else{
+        if (a>150000000){
+            cout<<"cost*0.45 -> "<<a*0.45;
+        }
     }
     return 0;
 
