f23d5e4683a5092c2acfd19e8a68c969b5e556d1
pjy <nihoo4u@gmail.com>
2024-01-24 17:08:43 +0900

    switch

--- day01.cpp
+++ day01.cpp
@@ -9,18 +9,16 @@ using namespace std;
 
 
 int main(void){
-    unsigned int a,b,c,sum;
-    double m,x;
-    cin>>a>>b>>c;
-    cout<<fixed;
-    sum=a+b+c;
-    m=sum/3;
-    x=((a-m)*(a-m)+(b-m)*(b-m)+(c-m)*(c-m))/3;
-    cout<<"합은: "<<sum<<endl;
-    cout<<"평균은 "<<m<<endl;
-    cout<<"분산은 "<<setprecision(3)<<x<<endl; 
+    int a;
+    cin>>a;
+    switch(a){
+        case 0: cout<<0<<endl;
+        case 1: cout<<1<<endl;
+        case 2: cout<<2<<endl;
+        case 3: cout<<3<<endl;
 
-
+    }
+    return 0;
 
 
 
