1a9e58fd0544c2a78d8043c466d3c33bd0b96181
pjy <nihoo4u@gmail.com>
2024-01-24 17:50:43 +0900

    calculate_days

--- day01.cpp
+++ day01.cpp
@@ -9,18 +9,44 @@ using namespace std;
 
 
 int main(void){
-    int a;
-    cin>>a;
-    cout<<fixed<<setprecision(2);
-    if (a>300000000){
-        cout<<"cost*0.5 -> "<<a*0.5;
+    int month;
+    int day=0;
+    int m1=31;
+    int m2=28;
+    int m3=31;
+    int m4=30;
+    int m5=31;
+    int m6=30;
+    int m7=31;
+    int m8=31;
+    int m9=30;
+    int m10=31;
+    int m11=30;
+    int m12=28;
+
+  
+    cin>>month;
+    switch(month){
+        case 12 : day+=m12;
+        case 11 : day+=m11;
+        case 10 : day+=m10;
+        case 9 : day+=m9;
+        case 8 : day+=m8;
+        case 7 : day+=m7;
+        case 6 : day+=m6;
+        case 5 : day+=m5;
+        case 4 : day+=m4;
+        case 3 : day+=m3;
+        case 2: day+=m2;
+        case 1 : day+=m1;
+        
+
+       
+
 
     }
-    else{
-        if (a>150000000){
-            cout<<"cost*0.45 -> "<<a*0.45;
-        }
-    }
+    cout<<day;
+    
     return 0;
 
 
