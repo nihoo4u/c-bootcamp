1489cae9cfea6ffd2b51e5b0607023b5a53ec923
pjy <nihoo4u@gmail.com>
2024-01-25 10:40:47 +0900

    do_while

--- day03.cpp
+++ day03.cpp
@@ -1,25 +1,31 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
-int day=0;
-int startday;
+int c;
 
 int main(){
-    cout<<"날짜 수를 입력하세요"<<endl;
-    cin>>day;
-    cout<<"첫 날의 요일을 입력하세요"<<endl;
-    cin>>startday;
-    cout<<"Sun Mon Tue Wed Thr Fri Sat"<<endl;
-    for(int j=0;j<startday;j++)
-        cout<<"    ";
-    for(int i=1;i<=day;i++){
-        startday++;
-        cout<<setw(3)<<i<<" ";
-        if(startday%7==0){
-            cout<<endl;
+    
+    
+    do{
+        cout<<"input the number"<<endl;
+        cin>>c;
+        if(c==1)
+            cout<<"1은 소수도 아니고 복소수도 아닙니다"<<endl;
+        else{
+            cout<<"다시 입력하세요"<<endl;
         }
     }
+    while(c<=1);
+    for(int i=2;i<c;i++){
+        if(c%i==0){
+            cout<<"소수가 아닙니다"<<endl;
+            return 0;
+        }
+            
+    }
+    cout<<"소수입니다"<<endl;
     return 0;
+    
 
 
     }
