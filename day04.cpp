a0fe2614bd2b496a7017dece66447efcc2c9b695
pjy <nihoo4u@gmail.com>
2024-01-26 18:18:35 +0900

    bank system

--- day04.cpp
+++ day04.cpp
@@ -1,54 +1,48 @@
 #include <iostream>
 using namespace std;
-float pi=3.14;
-class Circle{
+class Bank{
     private:
-        double radius;
+        int money;
+        static int count;
     public:
-        Circle(double x);
-        Circle(Circle& z);
-        // ~Circle();
-        void sett_radius();
-        double get_radius();
-        double get_perimeter();
-        double get_area();
+        Bank(int x);
+        int get_money();
+        int add_moeny(int x);
+        int subtract_monet(int x);
+        static int get_count();
 };
-double Circle::get_radius(){
-    return radius;
-}
-double Circle::get_perimeter(){
-    return 2*pi*radius;
-}
-double Circle::get_area(){
-    return pi*radius*radius;
-}
-void Circle::sett_radius(double x){
-    radius=x;
-}
-Circle::Circle(double x)
-:radius(x){
-    cout<<"circle1 생성자가 호출되었습니다"<<endl;
-}
-Circle::Circle(Circle& z):radius(z.radius*10){
-    cout<<"circle 2 생성자가 호출되었습니다"<<endl;
-}
 
+Bank::Bank(int x):money(x){
+    cout<<"bank program started"<<endl;
+}
+int Bank::get_money(){
+    count++;
+    return money;
+}
+int Bank::add_moeny(int x){
+    money+=x;
+    return money;
+}
+int Bank::subtract_monet(int x){
+    money+=x;
+    return money;
+}
+int Bank::get_count(){
+    return count;
+}
+int Bank::count=0;
 
 
 int main(){
-    Circle circle1(5.2);
-    Circle circle2(circle1);
-    circle1.sett_radius(4.5);
-
-    cout<<"circle1  "<<circle1.get_radius()<<endl;
-    cout<<"circle1  "<<circle1.get_perimeter()<<endl;
-    cout<<"circle1  "<<circle1.get_area()<<endl<<endl;
-    cout<<"circle2  "<<circle2.get_radius()<<endl;
-    cout<<"circle2  "<<circle2.get_perimeter()<<endl;
-    cout<<"circle2  "<<circle2.get_area()<<endl;
+    Bank bank1(30);
+    cout<<"은행잔고는"<<bank1.get_money()<<", count:"<<bank1.get_count()<<endl;
+    bank1.add_moeny(20);
+    cout<<"은행잔고는"<<bank1.get_money()<<", count:"<<Bank::get_count()<<endl;
+   
 
 
-    return 0;
+
+    
 
     
 }
