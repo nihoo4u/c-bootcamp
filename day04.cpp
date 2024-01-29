27201cc396aaa361cb6b2e8e880fe5e5801a220e
pjy <nihoo4u@gmail.com>
2024-01-29 15:05:21 +0900

    change array using cuntion

--- day04.cpp
+++ day04.cpp
@@ -3,20 +3,29 @@
 using namespace std;
 
 
-
+void get(int x[2][4],int y[4][2],int& t);
 int main(){
     int sum=0;
 
-    int x[5]={1,2,3,4,5};
-    int* t=x;
-
-    for(int i=0;i<5;i++){
-        sum+=*(t++);
+    int v[2][4]={{1,2,3,4},{5,6,7,8}};
+    int m[4][2];
+    get(v,m,sum);
+    for(int i=0;i<4;i++){
+        for(int j=0;j<2;j++){
+            cout<<m[i][j]<<" ";
+        }
+        cout<<endl;
     }
-    cout<<sum<<endl;    
     }
 
-
+void get(int x[2][4],int y[4][2],int& t){
+    for(int i=0;i<2;i++){
+        for(int j=0;j<4;j++){
+            y[j][i]=x[i][j];
+            t+=x[i][j];
+        }
+    }
+}
    
    
 
