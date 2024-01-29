db1db4f381566cad14d5c69c3ebbb362c0f38c42
pjy <nihoo4u@gmail.com>
2024-01-29 13:36:48 +0900

    real_reference

--- day04.cpp
+++ day04.cpp
@@ -5,23 +5,17 @@ using namespace std;
 
 
 int main(){
-    // int score=92;
-    // int& rscore=score;
-    // cout<<"score: "<<score<<endl;
-    // cout<<"rscore: "<<rscore<<endl;
-   
-    // int num=80;
-    // rscore=num;
-   
-    // cout<<"rscore: "<<rscore<<endl;
-    // cout<<"score: "<<score<<endl;
-    int score=90;
-    int rscore=80;
-    int temp;
-    temp=score;
-    score=rscore;
-    rscore=temp;
+    int score=92;
+    int& rscore=score;
     cout<<"score: "<<score<<endl;
     cout<<"rscore: "<<rscore<<endl;
+   
+    int num=80;
+    rscore=num;
+   
+    cout<<"rscore: "<<rscore<<endl;
+    cout<<"score: "<<score<<endl;
+    cout<<endl;
+
 
 }
