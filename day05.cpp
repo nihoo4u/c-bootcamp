2a14b654c7bafa10cab76197d82d82b12b23670e
pjy <nihoo4u@gmail.com>
2024-01-30 14:56:07 +0900

    dynamic allocation at 2-dimension array

--- day05.cpp
+++ day05.cpp
@@ -3,24 +3,31 @@
 
 using namespace std;
 
-void convey(int(*m)[3]);
+
 
 int main(){
-    int arr[2][3]={{10,20,30},{40,50,60}};
-    convey(arr);
+    int row_num;
+    int col_num;
+    cin>>row_num;
+    cin>>col_num;
+    int** p=new int* [row_num];
+    for(int i=0;i<col_num;i++){
+        p[i]=new int[col_num];
+    }
+    for(int i=0;i<row_num;i++){
+        for(int j=0;j<col_num;j++){
+            cout<<"p[i[[j]: "<<p[i][j]<<" ";
+        }
+        cout<<endl;
+    } 
+      
+
     
     
     
 }
 
-void convey(int(*m)[3]){
-    for(int i=0;i<2;i++){
-        for(int j=0;j<3;j++){
-            cout<<setw(3)<<m[i][j];
-        }
-        cout<<endl;
-    }
-}
+
 
 
 
